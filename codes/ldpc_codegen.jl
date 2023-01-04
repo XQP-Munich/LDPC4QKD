7fa076e90c55c520824f3f89177783b426eb08a6
Adomas Baliuka <A.Baliuka@physik.uni-muenchen.de>
2023-01-04 11:32:30 +0100

    Stores all matrices in new json file format. Updates README.
    LDPCStorage.jl now uses a new json-based format. This format is going to be used in this project from now on, as well as the new version of the Julia package.
    Generation of C++ header files is now also part of LDPCStorage.jl and the script `ldpc_codegen.jl` in this repository now only implements a command line interface.
    Note that the C++ simulator in this project still only supports parsing of binary matrices in the deprecated .cscmat files. This will be fixed later.

--- codes/ldpc_codegen.jl
+++ codes/ldpc_codegen.jl
@@ -10,21 +10,6 @@ using ArgParse
 using LDPCStorage  # custom project. Helper methods for reading and writing LDPC matrices to files
 
 
-header = raw"""
-// This is an automatically generated file.
-// A sparse LDPC matrix (containing only zeros and ones) is saved in compressed sparse column (CSC) format.
-// Since the matrix (and LDPC code) is known at compile time, there is no need to save it separately in a file.
-// This significantly blows up the executable size (the memory would still have to be used when saving the matrix).
-// The method seems to be reasonably fast (on a standard laptop).
-
-#include <cstdint>
-#include <array>
-
-namespace AutogenLDPC {
-
-"""
-
-
 struct ValidPath
     val::String
     ValidPath(s::AbstractString) = isfile(s) ? new(s) : error("No such file: '$s'")
@@ -41,17 +26,15 @@ function parse_my_args(args)
         "--input_code_path"
             required = true
             arg_type = ValidPath
-            help = "Path to .alist or .cscmat file containing the LDPC code. 
-                For .cscmat files, both qc-exponents as well as raw LDPC storage is supported."
+            help = "Path to .alist or .json file containing the LDPC code.
+                For .bincsc.json files, raw LDPC storage is supported.
+                For qccsc.json files, quasi-cyclic-exponents are supported"
         "--output_path"
             default = "autogen_ldpc_matrix_csc.hpp"
             arg_type = String
             help = "Path to put the automatically generated C++ file containing the LDPC code.
             If the file extension is .cscmat, a compressed sparse column storage of the full matrix
             (actual binary matrix, not QC-exponents) is written instead."
-        "--debug_use_only_part"
-            help = "If true, only a very small part of the provided matrix is processed."
-            action = :store_true
     end
 
     parsed_args = parse_args(args, s)
@@ -61,84 +44,7 @@ function parse_my_args(args)
     end
 
     return abspath(parsed_args["input_code_path"].val),
-            abspath(parsed_args["output_path"]),
-            parsed_args["debug_use_only_part"]
-end
-
-
-function write_cpp_constexpr_CSC(
-    H::AbstractArray{Int8, 2}, 
-    output_cpp_path::AbstractString
-    ; only_debug_mode = false
-    )
-    if only_debug_mode  # make matrix smaller...
-        cut_at = minimum([10, size(H)...])
-        H = sparse(H[1:ceil(Int, cut_at/2), 1:cut_at])
-    else
-        H = sparse(H)
-    end
-
-    _, _, values = findnz(H)
-
-    all(values .== 1) || throw(ArgumentError("Expected matrix containing only zeros and ones."))
-
-    num_nonzero = length(values)
-    if log2(num_nonzero) < 16
-        colptr_cpp_type = "std::uint16_t"
-    elseif log2(num_nonzero) < 32
-        colptr_cpp_type = "std::uint32_t"
-    elseif log2(num_nonzero) < 64
-        colptr_cpp_type = "std::uint64_t"
-    else
-        throw(ArgumentError("Input matrix not sparse? Has $num_nonzero entries..."))
-    end
-
-    if log2(size(H, 1)) < 16
-        row_idx_type = "std::uint16_t"
-    else 
-        row_idx_type = "std::uint32_t"
-    end
-
-    open(output_cpp_path, "w+") do f
-        print(f, header)
-
-        N = size(H, 2)
-
-        println(f, """
-        constexpr inline std::size_t M = $(size(H, 1));
-        constexpr inline std::size_t N = $(size(H, 2));
-        constexpr inline std::size_t num_nz = $num_nonzero;
-
-        constexpr inline std::array<$colptr_cpp_type, N + 1> colptr = {""")
-
-        for (i, idx) in enumerate(H.colptr)
-            print(f, "0x$(string(idx - 1, base=16))")  # Convert index to base zero
-            if i != length(H.colptr)
-                print(f, ",")
-            end
-            if mod(i, 100) == 0
-                println(f, "")  # for formatting.
-            end
-        end
-        println(f, "\n};\n")
-
-        println(f, "// ------------------------------------------------------- \n")
-        println(f, "constexpr inline std::array<$row_idx_type, num_nz> row_idx = {")
-
-        for (i, idx) in enumerate(H.rowval)
-            print(f, "0x$(string(idx - 1, base=16))")  # Convert index to base zero
-            if i != length(H.rowval)
-                print(f, ",")
-            end
-            if mod(i, 100) == 0
-                println(f, "")  # for formatting.
-            end
-        end
-        println(f, "\n};\n\n")
-
-        println(f, "} // namespace AutogenLDPC")
-    end
-
+            abspath(parsed_args["output_path"])
 end
 
 
@@ -195,26 +101,37 @@ end
 
 
 function main(args)
-    code_path, output_path, only_debug_mode = parse_my_args(args)
-    if LDPCStorage.file_extension(code_path) == ".alist"
-        H = load_alist(code_path)
-    elseif LDPCStorage.file_extension(code_path) == ".cscmat"
+    code_path, output_path = parse_my_args(args)
 
+    if endswith(code_path, ".alist")
+        H = load_alist(code_path)
+    elseif endswith(code_path, ".qccsc.json")
+        H = LDPCStorage.load_ldpc_from_json(code_path; expand_qc_exponents_to_binary=true)
+    elseif endswith(code_path, ".bincsc.json")
+        H = LDPCStorage.load_ldpc_from_json(code_path; expand_qc_exponents_to_binary=false)
+    elseif endswith(code_path, ".cscmat")
+        @warn "The `.cscmat` file format is deprecated. Consider converting to the json based formats instead!"
         H = load_cscmat_standard_or_qc_exponents(code_path)
     else
-        @error "Unsupported input file at path $code_path. Expected file extension `.cscmat` or `.alist`."
+        throw(ArgumentError(
+            "Unsupported input file at path $code_path. Expected file extension `qccsc.json`,`bincsc.json`, `.cscmat` (deprecated) or `.alist`."))
     end
 
-    if LDPCStorage.file_extension(output_path) == ".hpp"
-        write_cpp_constexpr_CSC(H, output_path; only_debug_mode)
-    elseif LDPCStorage.file_extension(output_path) == ".cscmat"
-        save_to_cscmat(H, output_path; allow_omit_entries_if_only_stored_ones=true,)
+    if  endswith(output_path, ".hpp")
+        open(output_path, "w+") do io
+            LDPCStorage.print_cpp_header(io, H)
+        end
+    elseif endswith(output_path, ".cscmat")
+        LDPCStorage.save_to_cscmat(H, output_path; allow_omit_entries_if_only_stored_ones=true,)        
+    # elseif code_path_file_extension == ".qccsc.json"  # would need get expansion factor from input file.
+    elseif endswith(output_path, ".bincsc.json")
+        LDPCStorage.save_to_bincscjson(output_path, H)
     else
-        @error "Unsupported input file at path $output_path. Expected file extension `.hpp` or `.cscmat`."
+        throw(ArgumentError(
+            "Unsupported input file at path $output_path. Expected file extension `.hpp` or `.cscmat`."))
     end
 
-
-    @info "Saved output file ($(Base.stat(output_path).size) bytes) at '$(output_path)'."
+    @info "Saved output file ($(Base.stat(output_path).size / 1000.) kilobytes) at '$(output_path)'."
 end
 
 
