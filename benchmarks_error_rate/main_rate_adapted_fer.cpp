29cf3a669c5f3908e14fd33556933fbb573b918d
adomasbaliuka <52975890+adomasbaliuka@users.noreply.github.com>
2024-05-28 10:19:19 +0200

    Bugfix: remove obsolete template in main_rate_adapted_fer.cpp

--- benchmarks_error_rate/main_rate_adapted_fer.cpp
+++ benchmarks_error_rate/main_rate_adapted_fer.cpp
@@ -26,10 +26,9 @@ constexpr auto help_text =
 using namespace LDPC4QKD::CodeSimulationHelpers;
 
 
-template<typename colptr_t=std::uint32_t, // integer type that fits ("number of non-zero matrix entries" + 1)
-        typename idx_t=std::uint16_t>
+template<typename idx_t=std::uint16_t>
 std::pair<size_t, size_t> run_simulation(
-        const LDPC4QKD::RateAdaptiveCode<colptr_t, idx_t> &H,
+        const LDPC4QKD::RateAdaptiveCode<idx_t> &H,
         double p,
         std::size_t num_frames_to_test,
         std::mt19937_64 &rng,
